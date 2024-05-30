950b30a5e37711175e8d48b43d1f7b875f4db07d
LCS-woke <woke@lcs.on.ca>
2024-05-30 11:30:40 -0400

    Initial Commit

---
+++ Culminating Sports app/Culminating_Sports_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  Culminating_Sports_appApp.swift
+//  Culminating Sports app
+//
+//  Created by William Oke on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct Culminating_Sports_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
